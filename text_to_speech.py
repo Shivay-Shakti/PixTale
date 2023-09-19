c9e0c2b6c0f4901ed50950625841663237dd286e
Shivay Shakti <105371559+Shivay-Shakti@users.noreply.github.com>
2023-09-19 15:51:58 +0530

    Updated with comment and docstring text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,25 +1,42 @@
+# Importing the 'requests' library for making HTTP requests and 'os' for environment variables
 import requests
 import os
 
+# Define a function called 'text2speech' that takes a message string as input
 def text2speech(message):
+    """
+    Convert a text message to speech using the Hugging Face Text-to-Speech API.
+    
+    Parameters:
+    message (str): The text message to be converted to speech.
+    
+    Returns:
+    None: Writes the generated audio to a file named 'audio.flac'.
+    """
+    
+    # Define the API URL for the Hugging Face Text-to-Speech model
     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    
+    # Set up the Authorization header using the API key from the environment variable
     headers = {"Authorization": f"Bearer {os.environ.get('HUGGINGFACEHUB_API_KEY')}"}
+    
+    # Create a dictionary containing the text input
     payloads = {"inputs": message}
 
-    # Making a POST request to the Text-to-Speech API
+    # Make a POST request to the Hugging Face API
     response = requests.post(API_URL, headers=headers, json=payloads)
     
-    # Check if the status code indicates success (HTTP 200)
+    # Check if the response is successful (HTTP status code 200)
     if response.status_code == 200:
-        # If successful, write the audio content to a file
+        # If successful, write the audio content to an 'audio.flac' file
         with open('audio.flac', 'wb') as file:
             file.write(response.content)
     else:
-        # If not successful, print the status code and response content
+        # If the request failed, print the status code and response content
         print(f"Failed to convert text to speech. Status code: {response.status_code}")
         print(f"Response: {response.json()}")
 
- 
-#scenario=image2text("https://github.com/Shivay-Shakti/Image2Audio/blob/8bb59e51c19387693bdd0299c16c596ab59f647b/cats.jpg") #updating with remote file url for the path
-#story = generate_story(scenario)
-#text2speech(story)
+# Sample code for testing (commented out)
+# scenario=image2text("https://...") # Updating with remote file URL for the path
+# story = generate_story(scenario)
+# text2speech(story)
