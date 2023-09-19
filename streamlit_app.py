db509a167b6ec51a0afb3e7405cb253eedf62f98
Shivay Shakti <105371559+Shivay-Shakti@users.noreply.github.com>
2023-09-19 16:20:37 +0530

    Updated to accept openai credentials in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,63 @@
-# Importing required modules and functions
+# Import necessary modules and functions
 import streamlit as st
 from image_to_text import image2text
 from story_generation import generate_story
 from text_to_speech import text2speech
 
 def main():
-    """
-    Main function to run the Streamlit app.
-    """
-    # Configure the Streamlit app
+    """Main function to run the Streamlit app."""
+    # Configure the Streamlit page
     st.set_page_config(page_title="Visual Story Generator", page_icon="🧊", layout="centered", initial_sidebar_state="expanded")
 
-    # Sidebar for API credentials
+    # Sidebar for user API Credentials
     with st.sidebar:
         st.header("API Credentials")
+        
+        # For Hugging Face
+        st.markdown("[Get a Hugging Face API Key](https://huggingface.co/Salesforce/blip-image-captioning-base)")
         hugging_face_key = st.text_input("Hugging Face API Key", type="password")
-        # Add more fields for other APIs as needed
+        
+        # For OpenAI
+        st.markdown("[Get an OpenAI API Key](https://platform.openai.com/account/api-keys)")
+        openai_key = st.text_input("OpenAI API Key", type="password")
 
-    # Main header
+    # Validate API Keys
+    if not hugging_face_key or not openai_key:
+        st.warning("Please enter valid API keys for both Hugging Face and OpenAI to proceed.")
+        return
+
+    # Main Header
     st.header("Your Image Into Story")
 
-    # File uploader for image
+    # File Uploader
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
     if uploaded_file is not None:
-        # Debugging: print uploaded file details
-        print(uploaded_file)
-
-        # Read uploaded file as bytes
+        # Read and Display Image
         bytes_data = uploaded_file.getvalue()
-
-        # Save the uploaded image locally
-        with open("uploaded_image.jpg", "wb") as f:
-            f.write(bytes_data)
-
-        # Display uploaded image
         st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
 
-        # Convert image to text and generate a story
-        scenario = image2text("uploaded_image.jpg")  # Assuming this function is defined in another file
-        story = generate_story(scenario)  # Assuming this function is defined in another file
+        # Image to Text
+        with st.spinner("Converting image to text..."):
+            scenario = image2text("uploaded_image.jpg")
+        
+        # Generate Story
+        with st.spinner("Generating story..."):
+            story = generate_story(scenario, openai_key)  # Assuming you modify generate_story to accept an API key
 
-        # Convert the generated story to speech
-        # Using the user-provided Hugging Face API key
-        text2speech(story, hugging_face_key)
+        # Text to Speech
+        with st.spinner("Converting text to speech..."):
+            text2speech(story, hugging_face_key)
 
-        # Display the generated text and story
+        # Display Scenario and Story
         with st.expander("Scenario"):
             st.write(scenario)
         with st.expander("Story"):
             st.write(story)
 
-        # Play the generated audio
+        # Play Audio
         st.audio("audio.flac")
 
-# Entry point of the application
+# Entry point for the script
 if __name__ == "__main__":
     main()
