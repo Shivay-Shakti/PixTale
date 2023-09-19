646c6517f5240c070728dfacf49a3186af6aed2a
Shivay Shakti <105371559+Shivay-Shakti@users.noreply.github.com>
2023-09-19 16:21:06 +0530

    Updated to accept openai credentials story_generation.py

--- story_generation.py
+++ story_generation.py
@@ -1,35 +1,22 @@
-# Importing necessary classes from the 'langchain' package
 from langchain.chat_models import ChatOpenAI as OpenAI
 from langchain import PromptTemplate, LLMChain
 
-# Define a function called 'generate_story' that takes a scenario string as input
-def generate_story(scenario):
+def generate_story(scenario, openai_api_key):
     """
-    Generate a short funny story based on a given scenario using the OpenAI GPT-3.5 Turbo model.
+    Generate a story based on the given scenario using OpenAI's API.
     
     Parameters:
-    scenario (str): The scenario or context for the story.
+    scenario (str): The context or scenario for the story.
+    openai_api_key (str): The OpenAI API key for authorization.
     
     Returns:
-    str: The generated story text.
+    str: The generated story.
     """
-    
-    # Define a template for the story prompt
-    # The template has a CONTEXT section to specify the scenario
     template = """You are a funny story teller;
     you can generate a short funny story of less than 100 words based on a simple narrative;
     CONTEXT : {scenario}
-    STORY:"""
-    
-    # Create a PromptTemplate object using the defined template
+    STORY:"""  
     prompt = PromptTemplate(template=template, input_variables=["scenario"])
-    
-    # Initialize the LLMChain object with the OpenAI GPT-3.5 Turbo model and the prompt
-    story_llm = LLMChain(llm=OpenAI(model_name="gpt-3.5-turbo", temperature=1), prompt=prompt, verbose=True)
-    
-    # Generate the story using the predict method of the LLMChain object
-    # The 'verify' parameter is set to False, meaning the prompt won't be verified before sending to the model
+    story_llm = LLMChain(llm=OpenAI(model_name="gpt-3.5-turbo", temperature=1, openai_api_key=openai_api_key), prompt=prompt, verbose=True)
     story = story_llm.predict(scenario=scenario, verify=False)
-    
-    # Return the generated story
     return story
