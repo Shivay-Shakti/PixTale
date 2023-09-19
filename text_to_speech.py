fd2ba89e5207f4d6ce2cd5d2436133e334e9cf2b
Shivay Shakti <105371559+Shivay-Shakti@users.noreply.github.com>
2023-09-19 16:22:11 +0530

    Updated to accept openai credentials text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,36 +1,25 @@
-# Importing the 'requests' library for making HTTP requests
 import requests
 
-def text2speech(message, api_key):
+def text2speech(message, hugging_face_key):
     """
-    Convert a text message to speech using the Hugging Face Text-to-Speech API.
+    Convert a text message to speech using Hugging Face's Text-to-Speech API.
     
     Parameters:
-    message (str): The text message to be converted to speech.
-    api_key (str): The Hugging Face API key for authorization.
+    message (str): The text to be converted to speech.
+    hugging_face_key (str): The Hugging Face API key for authorization.
     
     Returns:
-    None: Writes the generated audio to a file named 'audio.flac'.
+    None: Writes the generated audio to a file.
     """
-    
-    # Define the API URL for the Hugging Face Text-to-Speech model
     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    
-    # Set up the Authorization header using the provided API key
-    headers = {"Authorization": f"Bearer {api_key}"}
-    
-    # Create a dictionary containing the text input
+    headers = {"Authorization": f"Bearer {hugging_face_key}"}
     payloads = {"inputs": message}
 
-    # Make a POST request to the Hugging Face API
     response = requests.post(API_URL, headers=headers, json=payloads)
     
-    # Check if the response is successful (HTTP status code 200)
     if response.status_code == 200:
-        # If successful, write the audio content to an 'audio.flac' file
         with open('audio.flac', 'wb') as file:
             file.write(response.content)
     else:
-        # If the request failed, print the status code and response content
         print(f"Failed to convert text to speech. Status code: {response.status_code}")
         print(f"Response: {response.json()}")
